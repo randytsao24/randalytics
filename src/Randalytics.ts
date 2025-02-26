b80ceef247a5ad543a6e515425d9aa0d5efa7882
randytsao24 <randytsao24@gmail.com>
2025-02-25 19:32:14 -0500

    Add send event callback support

--- src/Randalytics.ts
+++ src/Randalytics.ts
@@ -15,20 +15,33 @@ interface ClickEvent extends AnalyticsEvent {
   text?: string;
 }
 
+type EventHandler = (event: AnalyticsEvent) => void | Promise<void>;
+
 export class Randalytics {
   private static instance: Randalytics;
-  private constructor() {}
+  private onEvent: EventHandler;
 
-  public static getInstance(): Randalytics {
+  private constructor(onEvent: EventHandler) {
+    this.onEvent = onEvent;
+  }
+
+  public static getInstance(onEvent?: EventHandler): Randalytics {
     if (!Randalytics.instance) {
-      Randalytics.instance = new Randalytics();
+      if (onEvent) {
+        Randalytics.instance = new Randalytics(onEvent);
+      } else {
+        const defaultHandler = (event: AnalyticsEvent) => {
+          console.log("Randalytics event:", event);
+        };
+        Randalytics.instance = new Randalytics(defaultHandler);
+      }
     }
+
     return Randalytics.instance;
   }
 
-  private trackEvent(event: AnalyticsEvent): void {
-    // TODO: Send event to backend
-    console.log("Analytics Event:", event);
+  private sendEvent(event: AnalyticsEvent): void {
+    this.onEvent(event);
   }
 
   public trackPageView(): void {
@@ -38,7 +51,8 @@ export class Randalytics {
       path: window.location.pathname,
       title: document.title,
     };
-    this.trackEvent(event);
+
+    this.sendEvent(event);
   }
 
   public trackClick(element: HTMLElement): void {
@@ -49,7 +63,8 @@ export class Randalytics {
       elementClass: element.className || undefined,
       text: element.textContent || undefined,
     };
-    this.trackEvent(event);
+
+    this.sendEvent(event);
   }
 
   public init(): void {
